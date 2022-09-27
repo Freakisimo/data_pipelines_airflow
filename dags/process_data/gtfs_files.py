91452c8ddaf84f7fe2d5ad5f39d6576c2309fd7c
dante7 <dante.arkade7@gmail.com>
2022-09-27 16:36:15 -0500

    Add process files gtfs

--- dags/process_data/gtfs_files.py
+++ dags/process_data/gtfs_files.py
@@ -12,7 +12,7 @@ default_args = {
 @dag(
     dag_id='process_gtfs_data',
     default_args=default_args,
-    start_date=datetime(2022,8,29),
+    start_date=datetime(2022,9,7),
     schedule_interval='@daily'
 )
 def process_gtfs_data():
@@ -23,9 +23,22 @@ def process_gtfs_data():
 
     @task
     def process_data():
-        dfs = {name: dfs3f.get_df(bucket, name) for name in csv_files}
-        for df in dfs.values():
-            print(df.head())
+        # dfs = {name: dfs3f.get_df(bucket, name) for name in csv_files}
+        # for df in dfs.values():
+        #     print(df.head())
+        agency_df = dfs3f.get_df(bucket, 'agency')
+        shapes_df = dfs3f.get_df(bucket, 'shapes')
+        routes_df = dfs3f.get_df(bucket, 'routes')
+        stops_df = dfs3f.get_df(bucket, 'stops')
+        stop_times_df = dfs3f.get_df(bucket, 'stop_times')
+        trips_df = dfs3f.get_df(bucket, 'trips')
+
+        print(agency_df.columns)
+        print(shapes_df.columns)
+        print(routes_df.columns)
+        print(stops_df.columns)
+        print(trips_df.columns)
+        print(stop_times_df.columns)
         return None
 
 
